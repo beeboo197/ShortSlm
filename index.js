f097569a46ed4e27722ced38ee9728aff74889ec
beeboo197 <160324997+beeboo197@users.noreply.github.com>
2024-04-22 12:06:36 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const bot = new Bot(process.env.BOT_TOKEN);
 
 
 bot.on('message', async (ctx) => {
-  const affId = "1bbe3f99e1586c1f63a229a6d98a2e32"
+  const affId = "1c63daf38439dc4cc204f6980381a312"
   const message = ctx.message.text;
   const linkRegex = /(https?:\/\/[^\s]+)/gm;
   const lzd = 'https://s.lazada'
